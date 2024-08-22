3f016e623c6925c2f7892d2edce44ca62a36efcd
Darth Degen <darthdegen@gmail.com>
2024-08-22 10:57:44 -0700

    axolotl page content

--- src/components/templates/AxolotlView.tsx
+++ src/components/templates/AxolotlView.tsx
@@ -22,15 +22,18 @@ const axolotlInfo: AxolotlInfo[] = [
     title: "the axies need our help",
     src: "dropdown-1.jpg",
     description: [
-      "These filters will funnel water directly from the lake where it begins a 4 step filtration system through various plants, and volcanic rock.",
-      "The goal being to eventually amass a body of water clean enough to re-introduce the Axolotls held in captivation back into the wild.",
+      "Lake Xochimilco is home to hundreds of tourists a day, and has been for quite some time now.",
+      "While it's a beautiful experience, this is the last part of the world where Axolotls are known to inhabit.",
+      "Due to the lakes heavy pollution, and introduction of foreign fish species, over time the Axolotl's population has nearly vanished.",
     ],
   },
   {
     title: "the axies need our help",
     src: "dropdown-1.jpg",
     description: [
-      "In order to preserve the species, many organizations like Somos Axolotl have begun to relocate these marvelous creatures into captivity where a consistent water quality, and safe environment can be provided.",
+      "Lake Xochimilco is home to hundreds of tourists a day, and has been for quite some time now.",
+      "While it's a beautiful experience, this is the last part of the world where Axolotls are known to inhabit.",
+      "Due to the lakes heavy pollution, and introduction of foreign fish species, over time the Axolotl's population has nearly vanished.",
     ],
   },
   {
@@ -47,10 +50,9 @@ const axolotlInfo: AxolotlInfo[] = [
     title: "moving the needle",
     src: "dropdown-1.jpg",
     description: [
-      "In order to preserve the species, many organizations like Somos Axolotl have begun to relocate these marvelous creatures into captivity where a consistent water quality, and safe environment can be provided.",
-      "On the exact same Chinampa where the Axolotl Statues are installed, our team has begun the development of organic bio-filters.",
-      "These filters will funnel water directly from the lake where it begins a 4 step filtration system through various plants, and volcanic rock.",
-      "The goal being to eventually amass a body of water clean enough to re-introduce the Axolotls held in captivation back into the wild.",
+      "In collaboration with the Solana Foundation and Sphere we have begun development on a donation system that accepts major currencies including crypto through Solana.",
+      "By the simple scan of a QR code installed on the Axolotl statues, you will be able to send funds within seconds to everyone involved.",
+      "These donations are automatically split 3 ways and distributed in real time to the Somos Axolotl organization, the owners and drivers of the boats on Lake Xochimilco, and the Chinampa owners.",
     ],
   },
 ];
@@ -105,27 +107,17 @@ const AxolotlView: FC<Props> = (props: Props) => {
         </div>
         {/* images */}
         <div className="w-full flex flex-col xs:flex-row xl:flex-nowrap justify-center gap-5 xl:justify-between overflow-hidden pt-5">
-          <Image
-            src={`/images/about/card-5.jpg`}
-            width={500}
-            height={500}
-            alt="Panda"
-            className="xs:w-[32%] xs:rounded-br-[100px]"
-          />
-          <Image
-            src={`/images/about/card-5.jpg`}
-            width={500}
-            height={500}
-            alt="Panda"
-            className="xs:w-[32%] xs:rounded-br-[100px]"
-          />
-          <Image
-            src={`/images/about/card-5.jpg`}
-            width={500}
-            height={500}
-            alt="Panda"
-            className="xs:w-[32%] xs:rounded-br-[100px]"
-          />
+          {Array.from({ length: 3 }).map((_, i) => (
+            <div key={i} className="xs:w-[32%]">
+              <Image
+                src={`/images/axolotl/col-${i + 1}.jpg`}
+                width={500}
+                height={500}
+                alt="Panda"
+                className="xs:rounded-br-[60px] lg:rounded-br-[100px]"
+              />
+            </div>
+          ))}
         </div>
         {/* pattern svg */}
         <div className="w-full flex justify-center ">
