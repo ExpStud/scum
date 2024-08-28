312697f0c5f7d0a9c0f85dbc5efa32ab6ae0b2ba
Darth Degen <darthdegen@gmail.com>
2024-08-28 08:35:54 -0700

    added slime descriptions

--- src/constants/collection.ts
+++ src/constants/collection.ts
@@ -1,7 +1,5 @@
 import { Collection } from "@types";
 
-const sample = "Leader of the Slimes. Best friend named Paco. Love's drawing, hip hop and movies. Once defeated a jaguar in hand to hand combat.";
-
 export const collection: Collection[] = [
   {
     id: 0,
@@ -9,7 +7,7 @@ export const collection: Collection[] = [
     tag: "scum",
     color: "#B3CCC3",
     mintAddress: "7fbbpVBnqhYeDbNRmYZ6R18FzWgesX9Ks94oooxJgNu9",
-    description: sample
+    description: "Leader of the slimes. Best friend named Paco. Love's drawing, hip hop and movies. Once defeated a jaguar in hand to hand combat."
   },
   {
     id: 1,
@@ -17,7 +15,7 @@ export const collection: Collection[] = [
     tag: "coyote",
     color: "#356551",
     mintAddress: "F1XAgf3FPQNNQ3aVows48RH2ShwYrGxfSYYbDApAAjCN",
-    description: sample
+    description: "Coyote the healer and warrior."
   },
   {
     id: 2,
@@ -25,7 +23,7 @@ export const collection: Collection[] = [
     tag: "el-guapo",
     color: "#FE6B2F",
     mintAddress: "J8oj2EudAry8bxCudxo1yXxVCTBtW954M1Ph5areu4iM",
-    description: sample
+    description: "Mysterious. Dangerous. Donates art supplies to children. Doesn't speak unless it's important."
   },
   {
     id: 3,
@@ -33,7 +31,7 @@ export const collection: Collection[] = [
     tag: "mayahuel",
     color: "#90CFBC",
     mintAddress: "uqNRKUoQJdUYPzWhz9uuCZAXWsPU5A5SoU7qYjuCYQF",
-    description: sample
+    description: "Slimes goddess of violent protest. Reigning arm(s) wrestling champion."
   },
   {
     id: 4,
@@ -41,7 +39,7 @@ export const collection: Collection[] = [
     tag: "smiley",
     color: "#5A4E42",
     mintAddress: "CwnG1kRzJN7GwcziQo4Hx1pbSQmnBMuy6mwcWFmg9ynw",
-    description: sample
+    description: "Does NOT smile."
   },
   {
     id: 5,
@@ -49,7 +47,7 @@ export const collection: Collection[] = [
     tag: "wednesday",
     color: "#81a2e8",
     mintAddress: "8ZDf1adDisgDrKp8nJccQ98LF22FkuDq1Vek1b8YsLY",
-    description: sample
+    description: "She does practice Santaria. She does have a crystal ball."
   },
   {
     id: 6,
@@ -57,7 +55,7 @@ export const collection: Collection[] = [
     tag: "thumper",
     color: "#2A4F43",
     mintAddress: "GX6XWg6vJ7iW1H6kR9q8TTG3E1xRzRCyeYTnNckpYBTS",
-    description: sample
+    description: "Harnessing ancient technology for fashion. Do not give Thumper daps if you value your hands."
   },
   {
     id: 7,
@@ -66,7 +64,7 @@ export const collection: Collection[] = [
     color: "#333333",
     doublePfp: true,
     mintAddress: "5wUmNTLcH7wWhU9GvgYRydtcaEdBvGbaqDSwk5Xhgj2D",
-    description: sample
+    description: "Junior is pretty quiet. Appears to be royalty. We weren't sure he could actually speak without his bottom jaw, but turns out he has a lovely singing voice. Junior is an adrenaline junky. Ironic considering his body can't produce adrenaline anymore..."
   },
   {
     id: 8,
@@ -74,7 +72,7 @@ export const collection: Collection[] = [
     tag: "temper",
     color: "#FBC6B0",
     mintAddress: "EMmTa2PwfCvpu6ZpwRtWVZ4B8mojw2WbJUdoJX68A8oB",
-    description: sample
+    description: `Temper is the enforcer. A heart of gold, and short fuse. Donates to animal shelters every time someone "gets hurt." Enjoys oil painting, bare knuckle boxing and video games.`
   },
   {
     id: 9,
@@ -82,7 +80,7 @@ export const collection: Collection[] = [
     tag: "zeke",
     color: "#8D9FC3",
     mintAddress: "G4ZRCjbvTqdWRBG7ZU4Wmo6bimbvQSVn7GfF3f7vQzH5",
-    description: sample
+    description: "Does not speak. Says a lot."
   },
   {
     id: 10,
@@ -90,7 +88,7 @@ export const collection: Collection[] = [
     tag: "rabbit",
     color: "#D5738F",
     mintAddress: "D4ABjRYWFopTKWxnsYbhL3KKALcCjs5UJn7yZXWVaoSB",
-    description: sample
+    description: "When other kids were eating playdough, Rabbit was eating Legos. Luxury stolen watch collector from age 6."
   },
   {
     id: 11,
@@ -98,7 +96,7 @@ export const collection: Collection[] = [
     tag: "flea",
     color: "#555647",
     mintAddress: "3SeqwjmtXHhzupPoMfQMVBYEghaAYksCY8jaYG7WJ5DV",
-    description: sample
+    description: "Flea and his Shadow Axolotl live at the border of the living and the not quite dead yet. Every gate needs it's keeper."
   },
   {
     id: 12,
@@ -107,7 +105,7 @@ export const collection: Collection[] = [
     tag: "lindo",
     color: "#B93D29",
     mintAddress: "4eb1TajEfpRngXLCdRgn9sMSYvKPWM1jW87TEHRjQ1e1",
-    description: sample
+    description: "Lindo is quite lovely once you get to know him. Avid collector of the ornaments of religion. Not really a heretic, just more of a heathen. Lindo is a member of the old tribe."
   },
   {
     id: 13,
@@ -115,7 +113,7 @@ export const collection: Collection[] = [
     tag: "nacho",
     color: "#8BD3D5",
     mintAddress: "GKYxNwdUbE6cuYx6S7PABemVvvzFNEbwGzmAbCEdEqX3",
-    description: sample
+    description: "Slime's official bouncer. Power-ups include: Breakfast burritos, futbol jerseys and watching a chef do knife tricks. Always accompanied by his faithful sidekick, Nena. Will bonk your head if provoked. Also he's taken. Sorry ladies (and fellas)."
   },
   {
     id: 14,
@@ -123,7 +121,7 @@ export const collection: Collection[] = [
     tag: "canelo",
     color: "#FFB969",
     mintAddress: "BJERNpc7vEM8aiZjkq7os4K3zVKWVtXjmFdfQamCPXdQ",
-    description: sample
+    description: "Heart of gold. Fists harder than Abuelita. Touch softer than DeLarosa."
   },
   {
     id: 15,
@@ -131,7 +129,7 @@ export const collection: Collection[] = [
     tag: "el-santo",
     color: "#3969A2",
     mintAddress: "F49BxnaUJeGRnb3kAxqLyRTiQ9MVKZr8HQhME6AHyxjw",
-    description: sample
+    description: "El santo smells like fresh chopped wood and your first kiss. Once defeated a bear in combat with a well timed wink. Often mistaken for a bronze statue of himself. Has elemental powers. Axolotl companion has never met a safe it couldn't crack."
   },
   {
     id: 16,
@@ -139,7 +137,7 @@ export const collection: Collection[] = [
     tag: "lobo",
     color: "#FFB969",
     mintAddress: "8eDha6YyBihJ8Ufq2SiEB88VewSmoFVfb2GCLrYiwmkV",
-    description: sample
+    description: `"Go then, there are other worlds than these." Lobo whispered, unsure which turn this was or how many were left.`
   },
   {
     id: 17,
@@ -147,7 +145,7 @@ export const collection: Collection[] = [
     tag: "sonny",
     color: "#243250",
     mintAddress: "8pyv7FpMa91iJLwiEC3stBH7uewUFNq5p8nd25HcbnQk",
-    description: sample
+    description: "Sonny is powered by the pure combined energy of internet and muses. Less artificial intelligence, more applied universal consciousness. Loves Cypress Hill, Stanley Kubrick and his sword."
   },
   {
     id: 18,
@@ -156,7 +154,7 @@ export const collection: Collection[] = [
     color: "#251C18",
     doublePfp: true,
     mintAddress: "4JGxTczAPU2x4c7mAEMzweBwWSvwrZTV2KtQnLqFgkhN",
-    description: sample
+    description: `Chido is an immortal. Chido steals most everything he touches. Chido fosters cats and is active in protecting small ecosystems on the border being destroyed by "the fence."`
   },
   {
     id: 19,
@@ -164,7 +162,7 @@ export const collection: Collection[] = [
     tag: "flaco",
     color: "#FFCC00",
     mintAddress: "3voFwtNFSSPjfmAcPxSi9FmtU3DcA5Wc4Bn3F3h2zcU6",
-    description: sample
+    description: "Trouble. Not much else to say about Flaco."
   },
   {
     id: 20,
@@ -172,7 +170,7 @@ export const collection: Collection[] = [
     tag: "pablo",
     color: "#1E2738",
     mintAddress: "8Qraa9JYsDVZgBuaVEmUuwoHwQMaXdgSqksvim59jd9P",
-    description: sample
+    description: "Make's it rain. Literally. Some say he's the second coming of Tlaloc. Others say he only leaves the house when it rains. You decide."
   },
   {
     id: 21,
@@ -180,7 +178,7 @@ export const collection: Collection[] = [
     tag: "kahlo",
     color: "#B7765B",
     mintAddress: "59ir4HEsTCeEk3EpkTNECg2JCuxN3KWwEeEUC3QzLbvU",
-    description: sample
+    description: "Not known for being known. Kahlo is stronger alone, but tolerates the company of slimes. Collects teeth, turquoise and wildflowers."
   },
   {
     id: 22,
@@ -188,7 +186,7 @@ export const collection: Collection[] = [
     tag: "santi",
     color: "#F76C31",
     mintAddress: "2uAGapxfHT42psiLu5U3SzZQ9hyvVCYx6oTdaWqjE564",
-    description: sample
+    description: "Bitten by a radioactive skeleton, Santi was granted the super power of partial invisibility...of everything but half his body and none of his skeleton. Always sit to his left when eating."
   },
   {
     id: 23,
@@ -196,7 +194,7 @@ export const collection: Collection[] = [
     tag: "xolo",
     color: "#552433",
     mintAddress: "3XJhT7u9oHiXB77fLf6w5sHMa1aZrXSkALoV46XitJbD",
-    description: sample
+    description: "Xolotl was the brother of Quetzalcoatl. Cast into exile, Xolotl was transformed into the Water Monster, namesake of the incredible Axolotl. This is my interpretation of the god, in the universe in my head, that all slimes exist in."
   },
   {
     id: 24,
@@ -204,7 +202,7 @@ export const collection: Collection[] = [
     tag: "kai",
     color: "#58C1BF",
     mintAddress: "GhfmfuTG4zqtf1bPuJA655jgM1DSaMQyhq6wttsc4m29",
-    description: sample
+    description: "A revolutionary. Timeless and hard. Kai has a dark side. No one knows quite where he came from, but everyone knows he's from somewhere else. Family nonetheless."
   },
   {
     id: 25,
@@ -212,7 +210,7 @@ export const collection: Collection[] = [
     tag: "angel",
     color: "#FE9064",
     mintAddress: "A8tfprbYBR6QMDmvfDDUdXVzc75tc2Kc7vteUetx2Acm",
-    description: sample
+    description: "Dressed up in hand me down tees, hair done, bangs curled. Heart of gold, fists like rocks. Flaca don't talk papas. She takes those."
   },
   {
     id: 26,
@@ -220,7 +218,7 @@ export const collection: Collection[] = [
     tag: "mick",
     color: "#185E5E",
     mintAddress: "Ezm7edUhZ6sgnKrouPUdAu8iSTb4EiEaR18HN26XHsXd",
-    description: sample
+    description: "The first new member encountered after the battle. Floats between our world and the other. Mick chain smokes Lucky Strikes, knows everything about subterranean gardening and once staged a coup against the devil. Unsure source of voice, doesn't appear to have a neck."
   },
   {
     id: 27,
@@ -228,7 +226,7 @@ export const collection: Collection[] = [
     tag: "toci",
     color: "#FFB89C",
     mintAddress: "CK1EsPaAHEsEadDAbzJsz5cGRKPU6Ejc1BuNcviih6Br",
-    description: sample
+    description: `"Dream a little dream of me..."`
   },
   {
     id: 28,
@@ -236,7 +234,7 @@ export const collection: Collection[] = [
     tag: "ernie",
     color: "#FA9764",
     mintAddress: "25xQAcbdiSgCx8TUFM7Gxi8kqshvJfnxTrhvvfbFBpEL",
-    description: sample
+    description: "Ernie never leaves the house without his faithful companion Bert. Together they tend the crops, survey the land, maintain the subtle balance of good and evil at the gates separating this world from the other and generally just vibe. Nobody messes with Ernie."
   },
   {
     id:29,
@@ -244,7 +242,7 @@ export const collection: Collection[] = [
     tag: "vince",
     color: "#F99473",
     mintAddress: "5UfnXYun7jpkS9xoiZcCjLr9ZT8DNp9b47SVi9JYsVyt",
-    description: sample
+    description: "Vince is an adept Materia user. Will summon La Chancla, baphomet or La Llorona when not bopping foo's on their head with his spiked cactus bat. Looks sleepy, isn't. Just a wonderful calming presence. Makes tamales with his abuela every christmas."
   },
   {
     id:30,
@@ -252,7 +250,7 @@ export const collection: Collection[] = [
     tag: "guac",
     color: "#FAE8D2",
     mintAddress: "DQSHtB8yr9PuDKbJsM7Bcb6Nd6kfpYjsDrHPNzeR5Dbi",
-    description: sample
+    description: `GUAC AN ASTRAL PLANE HOPPING NOMAD. AN ANCIENT. CURSED WITH THE AMBIGUITY OF BEING A BIPEDAL AMPHIBIAN. SOME BELIEVE HE IS AN ALEBRIJE, OTHER'S MAINTAIN HE'S BEEN TRANSFORMED BY TIME, FURTHER THAN ANY AJOLOTE BEFORE HIM. IF THEY WEREN'T GOING EXTINCT, WOULD THEY INSTEAD EXIST ALONG SIDE US?`
   },
   {
     id:31,
@@ -260,15 +258,15 @@ export const collection: Collection[] = [
     tag: "cuchillo",
     color: "#E4594E",
     mintAddress:"NeP7eVJf84CnWWgRRKX4p9T2LNQryRcun3d3KBcqLQ1",
-    description: sample
+    description: `SCENE: "Issa knife." Words dripped from the depths of the mask when asked "what does your name mean?" "gang shit, that's all I'm on. Slatt!" was his curious reply to the simple question; "what is your favorite frozen yogurt flavor?" - To be read in the voice of David Attenborough.`
   },
-  {
+  { 
     id:32,
     name: "El Niño",
     tag: "el-niño",
     color: "#D79CF8",
     mintAddress:"F3Yr87EKnFvPB9KuDMNNmHJDpRyz2tkYQswytEMnnoLo",
-    description: sample
+    description: "They say he built the internet in a day, but decided to pursue particle colliders instead. Is consistently maintaining Super Saiyan level 3. Keeps his thermos at a toasty 69,000 degrees, but the kid is always cool. Usually too busy learning complex machine learning to actually enter the chat."
   },
   {
     id:33,
@@ -276,7 +274,7 @@ export const collection: Collection[] = [
     tag: "cucuy",
     color: "#8596A5",
     mintAddress:"bevDnB96DULZBdtQAkHMqR1X1hAkrXSHiRkXEhNDdLa",
-    description: sample
+    description: "Dark Arts bouncing and breaking leaving trails of lost love and nightmares. X marks the spot, but Santa Muerte spots the marks, and Cucuy holds the handle. Bright madness. Cold steaming kettle."
   },
   {
     id:34,
@@ -284,7 +282,7 @@ export const collection: Collection[] = [
     tag: "gomez",
     color: "#182324",
     mintAddress : "GFoXPYCqEdSuPXahQaCu91ZWX85FdDrSJxF69kgxMz4E",
-    description: sample
+    description: "Gomez has layers. Many intricate layers."
   },
   {
     id:35,
@@ -292,7 +290,7 @@ export const collection: Collection[] = [
     tag: "camila",
     color: "#2D3A43",
     mintAddress : "BtF8citw7D2THyT9oRVSNfVGDybRE2U4RLbouh4onw7d",
-    description: sample
+    description: "The observer. The master of light and shadow. Not of realms, only of the space between. Camila only eats Empanadas de Calabaza (because they're the best)."
   },
   {
     id:36,
@@ -300,7 +298,7 @@ export const collection: Collection[] = [
     tag: "lotto",
     color: "#E4B856",
     mintAddress : "DNLigFquSxPmzN26SkFBYCJS6nZSdo3HGNuu4t8jBnBu",
-    description: sample
+    description: "Warrior. Long days and Long nights, never ever giving up the fight."
   },
   {
     id:37,
@@ -308,7 +306,7 @@ export const collection: Collection[] = [
     tag: "tuco",
     color: "#5C463F",
     mintAddress : "DhMW1FGjepmw2XjjX1rr3uWUyoRiBMwf7YyG78v4BWMU",
-    description: sample
+    description: "Tuco has been working hard in the shadows. Building the infrastructure of the universe. Small projects, and heavy traffic, executed with ease. All of the Slimes, without realizing, benefit a little from the presence of Tuco. Might rob a bank though, and keep your bags of gold away from him if you want to keep them."
   },
   {
     id:38,
@@ -316,7 +314,7 @@ export const collection: Collection[] = [
     tag: "andres",
     color: "#33383C",
     mintAddress : "EFJ6MnHcX9B4XpZVgHPGezvMMgx11U7wC4p2yg4QrxCd",
-    description: sample
+    description: "What get's measured, get's improved. Andres taps the fabric of all to decipher and organize chaos. He's been know to work on the other's orbs, owned by Coyote and Guac respectively. Still incapable of understanding WHEN, let alone where they're from, they do seem intent on returning balance to the realms, beneath, between and above."
   },
   {
     id:39,
@@ -324,7 +322,7 @@ export const collection: Collection[] = [
     tag: "mateo",
     color: "#A17A2D",
     mintAddress : "3sTfT7KtrevA7gxcowo4Fsa7uuXwCCaKg6uxPVt4UwN8",
-    description: sample
+    description: "Clean. Calculated. All in things in order. Mis en place in human form. While most slimes contribute to the general chaos of their world; color, violence and culture - Mateo finds harmonies, patterns and legibility. Every family needs at least one. Su manzana favorita es la Macintosh."
   },
   {
     id:40,
@@ -332,7 +330,7 @@ export const collection: Collection[] = [
     tag: "sancho",
     color: "#5680BA",
     mintAddress : "5JffUW8Lh1Zk8WryjSiMD1rHh3uzsoAsCQiK8yAJQGqK",
-    description: sample
+    description: "In this place, the thin line that separates good from bad has the highest population. Duality requires polarity, and polarity requires big energy. Sancho came from somewhere else, but feels more like someone who never left. Sancho lives by clear tenets; Change for no one, never let anyone outwork you and respect time."
   },
   {
     id:41,
@@ -340,7 +338,7 @@ export const collection: Collection[] = [
     tag: "chapo",
     color: "#2D6BBA",
     mintAddress : "GZZ4ZkJ5icsY9pyPW92bCeC8sSRFkaQqiwn6hXznHWDU",
-    description: sample
+    description: "Chapo is known around the barrio as the tooth fairy. The current largest contributor to the Polanco tooth museum. As many of the Slimes are, Chapo is born from between. Some Slimes exhibit whispers of ancient traditions and deities - Chapo seems born of the conflict brought by boats, robes and piety. Has 4 chihuahuas, 3 cats and loves Michelob Ultra."
   },
   {
     id:42,
@@ -348,7 +346,7 @@ export const collection: Collection[] = [
     tag: "xavier",
     color: "#717E86",
     mintAddress : "EFDFUYhQw2mghcs58nqnnJFG7DcbG4Sxy2Vz7991WxNT",
-    description: sample
+    description: "Xavier is a dark horse. Hailing from lands beyond the land he calls home. A brilliant wit and either clairvoyant or connected. Doesn't really matter which. His role in the story is of the unlikely but ubiquitous mentor. Doling out goodwill in the form of information and hard truths. Rumors are he is actually JUAN SNOW, EL REY DEL NORTE - but we'll probably never know."
   },
   {
     id:43,
@@ -356,7 +354,7 @@ export const collection: Collection[] = [
     tag: "chamaco",
     color: "#FF2198",
     mintAddress : "36NjK16PZ5CVBGAhyHNapW8Qztgi97KryYHKZXNaPfjf",
-    description: sample
+    description: `The kid. Don't press him. Also answers to "Bam Bam."`
   },
   {
     id:44,
@@ -364,7 +362,7 @@ export const collection: Collection[] = [
     tag: "itztli",
     color: "#FF4601",
     mintAddress : "kSwQFLjpSzPJ8exqW1PCh8PxDGBpYJKYYuhXo2SKhRq",
-    description: sample
+    description: "Obsidian lord of the night. God of stone. Has 3 pomeranians, 2 axolotls and makes tamales with his mom every Christmas. Sibling to the god of the underworld, Mictlan."
   },
 ];
 
