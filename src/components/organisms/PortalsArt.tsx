175301791394f36c7a1523ec9ed5ebb78b97908b
Darth Degen <darthdegen@gmail.com>
2025-04-28 09:09:52 -0700

    added art section

--- src/components/organisms/PortalsArt.tsx
+++ src/components/organisms/PortalsArt.tsx
@@ -16,12 +16,44 @@ const PortalsArt: FC<Props> = ({ ...divProps }) => {
     >
       {/* Character Explorations */}
       <h4 className="mb-5 uppercase">Character Explorations</h4>
+      <Image
+        src={`${process.env.CLOUDFLARE_STORAGE}/images/scum/slimes-studio/portals/art/a1.png`}
+        width={760}
+        height={679}
+        alt="Character Explorations 1"
+      />
       {/* Character Explorations */}
       <h4 className="mt-10 mb-5 uppercase">Character Explorations</h4>
+      <Image
+        src={`${process.env.CLOUDFLARE_STORAGE}/images/scum/slimes-studio/portals/art/a2.png`}
+        width={760}
+        height={570}
+        alt="Character Explorations 2"
+      />
       {/* Character Sheet */}
       <h4 className="mt-10 mb-5 uppercase">Character Sheet</h4>
+      <Image
+        src={`${process.env.CLOUDFLARE_STORAGE}/images/scum/slimes-studio/portals/art/a3.png`}
+        width={760}
+        height={755}
+        alt="Character Sheet"
+      />
+      {/* Initial Explorations + Moodboard */}
+      <h4 className="mt-10 mb-5 uppercase">Initial Explorations + Moodboard</h4>
+      <Image
+        src={`${process.env.CLOUDFLARE_STORAGE}/images/scum/slimes-studio/portals/art/a4.png`}
+        width={760}
+        height={789}
+        alt="Initial Explorations + Moodboard"
+      />
       {/* Before / after */}
       <h4 className="mt-10 mb-5 uppercase">Before / after</h4>
+      <Image
+        src={`${process.env.CLOUDFLARE_STORAGE}/images/scum/slimes-studio/portals/art/a5.png`}
+        width={760}
+        height={400}
+        alt="Before After"
+      />
     </SlimesStudioToggleLayout>
   );
 };
